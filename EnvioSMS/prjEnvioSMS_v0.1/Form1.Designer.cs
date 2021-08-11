a601bf65a4dbd8f1fae3192515e60893538bfeab
Paulo Vinicius Dos Santos <61323580+jakerinha@users.noreply.github.com>
2021-08-10 21:10:48 -0300

    meu primeiro commit - Projeto Envio de SMS
    Testando API para envio de SMS da empresa COMTELE.

---
+++ EnvioSMS/prjEnvioSMS_v0.1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace prjEnvioSMS_v0._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(70, 81);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(134, 52);
+            this.btnEnviar.TabIndex = 0;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 206);
+            this.Controls.Add(this.btnEnviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ EnvioSMS/prjEnvioSMS_v0.1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Comtele.Sdk.Services;
+
+
+
+//using TextmagicRest;
+//using TextmagicRest.Model;
+
+namespace prjEnvioSMS_v0._1
+{
+    public partial class Form1 : Form
+    {
+        private static string API_KEY = "77036d95-4db8-4c4a-a5c2-e1731b8f444f";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EnviarSMS()
+        {
+            
+            var textMessageService = new TextMessageService(API_KEY);
+            var result = textMessageService.Send("", "Hello CSharp", new string[] { "15988281252", "15988146648", "15988330673" });
+
+            if (result.Success)
+            {
+                Console.WriteLine("A mensagem foi enviada com sucesso!");
+            }
+            else
+            {
+                Console.WriteLine("A mensagem não pode ser enviada. Detalhes: " + result.Message);
+            }
+            
+            
+            
+            /*var client = new Client("test","my-api-key");
+            var link = client.SendMessage("Hello from TextMagic API", "+5515988281252");
+            if (link.Success)
+            {
+                Console.WriteLine("Message ID {0} has been successfully sent", link.Id);
+            }
+            else
+            {
+                Console.WriteLine("Message was not sent due to following exception: {0}.", link.ClientException.Message);
+            }*/
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            EnviarSMS();
+        }
+    }
+
+}
